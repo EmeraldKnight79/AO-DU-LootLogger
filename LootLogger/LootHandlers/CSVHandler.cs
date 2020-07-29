6ffc5c4877128cdfe4d1cf58ec88a86411da6960
Reuben Swartz <rbn.swartz@gmail.com>
2020-07-29 07:44:28 -0400

    Added id to output file to help in the cases where ids don't match

--- LootLogger/LootHandlers/CSVHandler.cs
+++ LootLogger/LootHandlers/CSVHandler.cs
@@ -29,11 +29,13 @@ namespace LootLogger.LootHandlers
                     // Player
                     // Looted From
                     // Item
+                    // ItemID
                     // Amount
                     writer.WriteField("Timestamp");
                     writer.WriteField("Player");
                     writer.WriteField("Looted From");
                     writer.WriteField("Item");
+                    writer.WriteField("ItemId");
                     writer.WriteField("Amount");
                     foreach(var loot in AllLoot.Where(i => !i.IsTrash))
                     {
@@ -41,6 +43,7 @@ namespace LootLogger.LootHandlers
                         writer.WriteField(loot.LooterName);
                         writer.WriteField(loot.BodyName);
                         writer.WriteField(loot.ItemName);
+                        writer.WriteField(loot.Id);
                         writer.WriteField(loot.Quantity);
                         writer.NextRecord();
                     }
